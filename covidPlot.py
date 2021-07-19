78e4809df857770bd93fdcce008101c6e3d457d7
dillan1111 <70030386+dillan1111@users.noreply.github.com>
2021-07-19 16:08:36 -0400

    Add files via upload

--- covidPlot.py
+++ covidPlot.py
@@ -1,5 +1,13 @@
 import pandas as pd;
 import plotly_express as px;
 df = pd.read_csv("covid_Data.csv")
-fig = px.scatter(df, x = 'date', y = 'cases', color = 'country', title = 'Cases of Covid per date for many countries.')
+#fig = px.scatter(df, x = 'date', y = 'cases', size = 'pop', theme='plotly_dark', orientation='h', color = 'country', title = 'Cases of Covid per date for many countries.')
+
+for template in ["plotly", "plotly_white", "plotly_dark", "ggplot2", "seaborn", "simple_white", "none"]:
+    fig = px.scatter(df,
+                     x='date', y='cases', color='country',
+                     size_max=60,
+                     template=template, title="Cases of Covid per date for many countries: '%s' theme" % template)
+    fig.show()
+
 fig.show()
